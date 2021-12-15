c9fe300f43cc6401a074dbcbbf13e0086a1fd5a5
Steffen Hiller <6301+steffen@users.noreply.github.com>
2021-12-15 13:23:34 +0100

    Chagne from source to remote and remove remote token

--- mirror-sync.sh
+++ mirror-sync.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 git fetch --all
 
-source_branches=$(git branch --all | grep remotes/source/)
+remote_branches=$(git branch --all | grep remotes/source/)
 
-echo "Source branches:"
-echo "$source_branches"
+echo "Remote branches:"
+echo "$remote_branches"
 
-for source_branch_ref in $source_branches; do
-  source_branch_name=${source_branch_ref#remotes/source/}
+# Loop through all remote branches
+for remote_branch_ref in $remote_branches; do
+  # Get the remote branch name
+  remote_branch_name=${remote_branch_ref#remotes/source/}
   
-  # Checkout if source branch exists on mirror
-  git rev-parse --verify remotes/origin/"$source_branch_name"
+  # Checkout if remote branch exists on mirror
+  git rev-parse --verify remotes/origin/"$remote_branch_name"
   result_code=$?
 
-  # If branch exists on mirror, pull changes from source repository into it
-  # If branch does not exist on mirror, create it from source repository
+  # If branch exists on mirror, pull changes from remote repository into it
+  # If branch does not exist on mirror, create it from remote repository
   if [ $result_code -eq 0 ]; then
-    git checkout -B "$source_branch_name" remotes/origin/"$source_branch_name"
-    git pull --no-rebase --no-edit source "$source_branch_name"
+    git checkout -B "$remote_branch_name" remotes/origin/"$remote_branch_name"
+    git pull --no-rebase --no-edit remote "$remote_branch_name"
   else
-    git checkout -b "$source_branch_name" "$source_branch_ref"
+    git checkout -b "$remote_branch_name" "$remote_branch_ref"
   fi
 
-  git push origin "$source_branch_name"
-  git push source "$source_branch_name"
+  git push origin "$remote_branch_name"
+  git push remote "$remote_branch_name"
 done
